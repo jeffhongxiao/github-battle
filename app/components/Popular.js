5694b11bfe7237a76fd77709a60af356d9f6f76a
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-05-14 13:20:16 -0300

    data in this.state.repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,8 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
+const api = require('../utils/api');
+
 function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -31,22 +33,36 @@ class Popular extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        selectedLanguage: 'All'
+        selectedLanguage: 'All',
+        repos: null
       };
 
       this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function() {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
     });
+
+    api.fetchPopularRepos(lang)
+      .then(function(repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          }
+        })
+      }.bind(this));
   }
 
   render() {
-
 
     return (
       <div>
@@ -54,6 +70,7 @@ class Popular extends React.Component {
           selectedLanguage = {this.state.selectedLanguage}
           onSelect = {this.updateLanguage}
           />
+        {JSON.stringify(this.state.repos)}
       </div>
     );
   }
